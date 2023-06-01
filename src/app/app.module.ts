8e14b116697ff0ff8bcb25bf8bc55b7e75d727ed
Lucas Fidelis <fidelis.lucas@gmail.com>
2023-06-01 15:01:27 -0300

    creating base files and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeadingComponent } from './heading/heading.component';
+import { SearchboxComponent } from './searchbox/searchbox.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadingComponent,
+    SearchboxComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
